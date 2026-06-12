85542eafea665f846e46a125951866a7131cc820
mac <mhmdsmir90@gmail.com>
2026-06-12 15:58:34 +0300

    Reportttt

--- src/routes/studentRoutes.js
+++ src/routes/studentRoutes.js
@@ -2,28 +2,45 @@ const express = require("express");
 const router = express.Router();
 const studentController = require("../controllers/studentController");
 const { getAllLimiter, writeLimiter } = require("../middleWares/rateLimiter");
+const { verifyToken } = require("../middleWares/authMiddleware");
+
+const restrictToStaff = (req, res, next) => {
+  if (!req.user || (req.user.type !== "Manager" && req.user.type !== "Teacher")) {
+    return res.status(403).json({ message: "Access denied: Managers and Teachers only" });
+  }
+  next();
+};
+
 router.get("/", getAllLimiter, studentController.getAll);
 router.get("/:id", getAllLimiter, studentController.getById);
-router.post("/create-student", writeLimiter, studentController.create);
-router.put("/update-student/:id", writeLimiter, studentController.update);
-router.delete("/delete-student/:id", writeLimiter, studentController.remove);
+
+router.post("/create-student", verifyToken, restrictToStaff, writeLimiter, studentController.create);
+router.put("/update-student/:id", verifyToken, restrictToStaff, writeLimiter, studentController.update);
+router.delete("/delete-student/:id", verifyToken, restrictToStaff, writeLimiter, studentController.remove);
+
 // Reports
-router.post("/:id/reports", writeLimiter, studentController.addReport);
+router.post("/:id/reports", verifyToken, restrictToStaff, writeLimiter, studentController.addReport);
 router.get("/:id/reports", getAllLimiter, studentController.getReports);
 router.get("/:id/reports/:reportId", getAllLimiter, studentController.getReportById);
-router.put("/:id/reports/:reportId", writeLimiter, studentController.updateReport);
-router.delete("/:id/reports/:reportId", writeLimiter, studentController.deleteReport);
+router.put("/:id/reports/:reportId", verifyToken, restrictToStaff, writeLimiter, studentController.updateReport);
+router.delete("/:id/reports/:reportId", verifyToken, restrictToStaff, writeLimiter, studentController.deleteReport);
+
 // Assignments
-router.post("/:id/assignments", writeLimiter, studentController.addAssignment);
+router.post("/:id/assignments", verifyToken, restrictToStaff, writeLimiter, studentController.addAssignment);
 router.get("/:id/assignments", getAllLimiter, studentController.getAssignments);
 router.put(
   "/:id/assignments/:assignmentId",
+  verifyToken,
+  restrictToStaff,
   writeLimiter,
   studentController.updateAssignment,
 );
 router.delete(
   "/:id/assignments/:assignmentId",
+  verifyToken,
+  restrictToStaff,
   writeLimiter,
   studentController.deleteAssignment,
 );
+
 module.exports = router;
